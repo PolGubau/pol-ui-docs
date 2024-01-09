a680283fc35dff224ce27812327928e45061c3e5
Pol Gubau <gubaupol@gmail.com>
2024-01-09 20:25:54 +0100

    Update import and tooltip content in Snippet component

--- components/snippet/snippet.tsx
+++ components/snippet/snippet.tsx
@@ -1,8 +1,8 @@
 import type { FC, HTMLAttributes } from 'react';
 
 import { useState } from 'react';
+import { Button, Tooltip } from 'pol-ui';
 
-import {  Button, Tooltip } from 'pol-ui';
 import CheckIcon from '@components/icons/check';
 import CopyIcon from '@components/icons/copy';
 
@@ -16,7 +16,7 @@ const Snippet: FC<SnippetProps> = ({ className }) => {
   const [copied, setCopied] = useState(false);
 
   const handleCopyCode = () => {
-    navigator.clipboard.writeText('npm install tailwind-variants');
+    navigator.clipboard.writeText('npm i pol-ui');
     setCopied(true);
     setTimeout(() => {
       setCopied(false);
@@ -25,23 +25,34 @@ const Snippet: FC<SnippetProps> = ({ className }) => {
 
   return (
     <div
-      className={`flex gap-2 pb-2 w-full items-center justify-center  ${className ? ` ${className}` : ''}`}
-     >
-      <div className="dark:text-white max-w-[400px] bg-secondary-900 px-3 py-2 rounded-2xl">$ npm install tailwind-variants</div>
-       
-      
+      className={`flex w-full items-center justify-center gap-2 pb-2  ${
+        className ? ` ${className}` : ''
+      }`}
+    >
+      <div
+        className="rounded-2xl bg-secondary-900/30 px-3 py-2 dark:text-white"
+        style={{
+          zIndex: 50
+        }}
+      >
+        $ npm install pol-ui
+      </div>
 
-      <Tooltip content="Copy to clipboard"  animation={ `duration-${20}` }>
-        
-        <Button onClick={ handleCopyCode } className="flex items-center justify-center aspect-square rounded-full" color="secondary" size={'sm'}  >
-        {copied ? (
-          <CheckIcon className="text-success" size={18} />
-        ) : (
-          <CopyIcon
-            className="text-neutral-600 dark:text-neutral-400"
-            size={18}
-          />
-          ) }
+      <Tooltip animation={`duration-${20}`} content="Copy to clipboard">
+        <Button
+          className="flex aspect-square items-center justify-center rounded-full"
+          color="secondary"
+          size={'sm'}
+          onClick={handleCopyCode}
+        >
+          {copied ? (
+            <CheckIcon className="text-success" size={18} />
+          ) : (
+            <CopyIcon
+              className="text-neutral-600 dark:text-neutral-400"
+              size={18}
+            />
+          )}
         </Button>
       </Tooltip>
     </div>
